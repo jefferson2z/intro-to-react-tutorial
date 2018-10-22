a1b356cfd3ab04737d14e1260986b2d0f8485783
Jefferson Oliveira <jefferson2z@hotmail.com>
2018-10-22 14:41:25 +0200

    Function Components

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ class Square extends React.Component {
     return (
       <button
         className="square"
-        onClick={() =>  this.props.onClick()}
+        onClick={() =>  props.onClick()}
       >
-        {this.props.value}
+        {props.value}
       </button>
     );
   }
